7b3633f7be62a8145dc9e1801c30cce4f3f9337d
radoslavvkostadinov <rekostadinov@gmail.com>
2024-01-17 23:19:38 +0200

    Add API_KEY and trendingUrl constants

--- template/src/common/constants.js
+++ template/src/common/constants.js
@@ -1,6 +1,6 @@
 export const HOME = 'home';
 
-export const CATEGORIES = 'categories';
+export const TRENDING = 'trending';
 
 export const FAVORITES = 'favorites';
 
@@ -11,3 +11,8 @@ export const CONTAINER_SELECTOR = '#container';
 export const FULL_HEART = '❤';
 
 export const EMPTY_HEART = '♡';
+
+export const API_KEY = '8hn1LkoWv2wIkMKj670q5ad0SZ85dlOV';
+
+export const trendingUrl = `https://api.gihpy.com/v1/gifs/trending?api_key=${API_KEY}hn1LkoWv2wIkMKj670q5ad0SZ85dlOV&limit=${limit}&offset=${offset}&rating=g`;
+export const getTrendingUrl = (limit = 25, offset = 0) => `https://api.gihpy.com/v1/gifs/trending?api_key=${API_KEY}hn1LkoWv2wIkMKj670q5ad0SZ85dlOV&limit=${limit}&offset=${offset}&rating=g`
