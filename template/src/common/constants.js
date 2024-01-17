c431ec3c6e8581a7dedf8a924be56f8058f8fdab
StanislavHristov <stanislav.hristov,a55@learn.telerikacademy.com>
2024-01-17 23:52:52 +0200

    Trending Gifs

--- template/src/common/constants.js
+++ template/src/common/constants.js
@@ -1,6 +1,6 @@
 export const HOME = 'home';
 
-export const CATEGORIES = 'categories';
+export const TRENDING = 'trending';
 
 export const FAVORITES = 'favorites';
 
@@ -11,3 +11,10 @@ export const CONTAINER_SELECTOR = '#container';
 export const FULL_HEART = '❤';
 
 export const EMPTY_HEART = '♡';
+
+export const API_KEY = 'f8vCamsq9izeN75AajMzZs0gO6kvHgJf';
+
+export const trendingUrl = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=25&offset=0&rating=g`;
+
+export const getTrendingUrl = (limit = 25, offset = 0) => `https://api.giphy.com/v1/gifs/trending?api_key=
+${API_KEY}&limit=${limit}&offset=${offset}&rating=g`;
--- template/src/requests/request-service.js
+++ template/src/requests/request-service.js
@@ -1,4 +1,28 @@
 import { getCategories, getMoviesGeneralInfo, getMoviesFullInfo, getMovieById, getCategory, searchMovies } from '../data/movies.js';
+import { getTrendingUrl } from '../common/constants.js';
+/**
+ * 
+ * @returns {Promise<Array<{
+* id: string,
+* rating: string,
+* title: string,
+* images: {
+*  fixed_width: {
+*  url: string,
+* },
+* },
+* user: {
+*  avatar_url: string,
+*  username: string,
+* },
+* }>>}
+ */
+export const loadTrendingGifs = async() => {
+  const response = await fetch(getTrendingUrl(30));
+    const result = await response.json();
+
+    return result.data;
+};
 
 export const loadCategories = () => {
   const categories = getCategories();
