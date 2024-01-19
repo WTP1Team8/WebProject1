4aa601389e57805bcf346ec14fd76142b9470300
StanislavHristov <stanislav.hristov,a55@learn.telerikacademy.com>
2024-01-19 21:10:41 +0200

    cosmetic changes of text of basics view

--- template/src/views/about-view.js
+++ template/src/views/about-view.js
@@ -2,8 +2,8 @@ export const toAboutView = () => `
 <div id="about">
   <div class="content">
     <h1>About the app</h1>
-    <h2>Authors: Telerik Academy</h2>
-    <h2>Date: 2021</h2>
+    <h2 id="h2-text">Authors: ARS app</h2>
+    <h3 id="h3-text">Date:2024</h3>
   </div>
 </div>
 `;
--- template/src/views/home-view.js
+++ template/src/views/home-view.js
@@ -2,12 +2,12 @@ export const toHomeView = () => `
 <div id="home">
   <h1>Gif(S)ter</h1>
   <div class="content">
-    <p>Simple movie database app. You can:</p>
+    <p>Single page app</p>
     <ul>
       <li>Browse categories</li>
-      <li>Browse movies</li>
-      <li>Add and remove movies from favorites</li>
-      <li>Search for movies by title</li>
+      <li>Browse giphy's</li>
+      <li>Add and remove giphy from favorites</li>
+      <li>Search for giphy by title</li>
     </ul>
   </div>
 </div>
