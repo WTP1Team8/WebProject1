ec9dc889162355363cc949fc1ece9451fb2a56f0
StanislavHristov <stanislav.hristov,a55@learn.telerikacademy.com>
2024-01-22 21:26:28 +0200

    about text

--- template/src/views/about-view.js
+++ template/src/views/about-view.js
@@ -1,9 +1,12 @@
 export const toAboutView = () => `
-<div id="about">
-  <div class="content">
+<div class="text-about">
+<h1>About the app</h1>
+<h2>Authors: ARS app &#174;</h2>
+<h3>Date:2024</h3>
+</div>
+`;
+/* <div class="content">
     <h1>About the app</h1>
     <h2 id="h2-text">Authors: ARS app</h2>
     <h3 id="h3-text">Date:2024</h3>
-  </div>
-</div>
-`;
+  </div> */
